0c1e2691eeb66bfba7aff97541f1faaf2bf19416
MatheusGomes58 <69799035+MatheusGomes58@users.noreply.github.com>
2022-01-18 18:50:55 -0300

    Add files via upload

---
+++ AdmAcess/js/init.js
@@ -0,0 +1,12 @@
+// Such info must be obtained from Firebase Project, on firebase console
+var  firebaseConfig = {
+  apiKey: "AIzaSyCCaktuRXFT4g5g5ufNepgCblYmBs2cPOc",
+  authDomain: "clubedesbravadoresrigel.firebaseapp.com",
+  databaseURL: "https://clubedesbravadoresrigel-default-rtdb.firebaseio.com/",
+  projectId: "clubedesbravadoresrigel",
+  storageBucket: "clubedesbravadoresrigel.appspot.com",
+  messagingSenderId: "1039116151343",
+  appId: "1:1039116151343:web:506e347367cb0ff21274da",
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
